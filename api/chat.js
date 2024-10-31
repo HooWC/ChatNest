851760e6796a30b4bae4b6f5ff50821b3c6b6697
Hoo Weng Chin <wengchin123456@gmail.com>
2024-10-31 13:29:52 +0800

    update

--- api/chat.js
+++ api/chat.js
@@ -1,31 +1,35 @@
-// api/chat.js
 const mongoose = require('mongoose');
 const { Server } = require('socket.io');
+const Message = require('../models/Message'); // 引入 Message 模型
 
-const username = 'your_username'; // 替换为你的用户名
-const password = 'your_password'; // 替换为你的密码
-const dbName = 'sample_mflix';
-const uri = mongodb + srv://${username}:${password}@cluster0.5x4eq.mongodb.net/${dbName}?retryWrites=true&w=majority;
+const username = "wengchinbusiness";
+const password = "OdelKSTJEWDKD27T";
+const dbName = 'sample_mflix'; // 替换为你的数据库名称
+const uri = `mongodb+srv://${username}:${password}@cluster0.5x4eq.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
-    mongoose.connect(uri)
-        .then(() => console.log('Connected to MongoDB Atlas'))
-        .catch(err => console.error('Database connection error:', err));
+mongoose.connect(uri)
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch(err => console.error('Database connection error:', err));
 
 module.exports = (req, res) => {
-    if (req.method === 'GET') {
-        res.status(200).send('Chat API is working');
-    }
-
     const io = new Server(res.socket.server);
+
+    if (req.method === 'GET') {
+        Message.find().then(messages => {
+            res.status(200).json(messages); // 返回所有消息
+        }).catch(err => {
+            res.status(500).json({ error: 'Database error' });
+        });
+    }
 
     io.on('connection', (socket) => {
         console.log('A user connected');
 
         socket.on('chat message', async (msg) => {
             console.log('Message received: ' + msg);
-            // 这里保存消息到数据库的代码
-            // 然后广播消息到所有客户端
-            io.emit('chat message', msg);
+            const message = new Message({ content: msg });
+            await message.save(); // 保存消息到数据库
+            io.emit('chat message', msg); // 广播消息到所有客户端
         });
 
         socket.on('disconnect', () => {
